f57d73a796d05940810bf8dfd9506a99b2d02745
Edoardo Ferretto <edoardo.ferretto99@gmail.com>
2026-06-28 23:02:05 +0200

    better install

--- install.sh
+++ install.sh
@@ -20,36 +20,43 @@ VENV_DIR="${DEST_DIR}/.venv"
 [[ -d "$SRC_APP_DIR" ]] || { echo "Error: $SRC_APP_DIR directory not found."; exit 1; }
 command -v python3 >/dev/null 2>&1 || { echo "Error: python3 not found in PATH."; exit 1; }
 
-# Create destination directory "/usr/local/bin/sealed_src"
+echo "Installing Sealed..."
+
+echo "Create destination directory /usr/local/bin/sealed_src"
 # Delete it if it already exist
 # rm -rf -- "${DEST_DIR}"
 mkdir -p "$DEST_DIR"
 
-# Move "src" into the sealed_src folder
+echo "Installing application files"
 rm -rf -- "${DEST_DIR}/src"
 cp -r "$SRC_APP_DIR" "${DEST_DIR}"
 
 # Copy main.py (launcher) to destination as "sealed", do the same with the restore script
 # we save it as that because it's easier for sudoers file and "at" command just calling "sealed" instead of python sealed.py
+echo "Installing scripts"
 cp "$SRC_MAIN" "$DEST_APP"
 cp "$SRC_RESTORE" "$DEST_RESTORE"
 
+echo "Ensuring permissions"
 # Make the "sealed" and "restore" files executable
 chmod +x "$DEST_APP"
 chmod +x "$DEST_RESTORE"
 
+echo "Creating virtual environment"
 # Create virtual environment if missing (this could be omitted, but better if we will use libraries)
 rm -rf -- "$VENV_DIR"
 if [[ ! -d "$VENV_DIR" ]]; then
   python3 -m venv "$VENV_DIR"
 fi
 
+echo "Creating launcher shortcut"
 # Make a symlink so we can execute it by simply typing "sealed --block" instead of doing "usr/local/bin/..."
 ln -sf "$DEST_APP" /usr/local/bin/sealed
 
+echo "First run of Sealed, checking permissions..."
 "$DEST_APP" --check-sudoers
 
-echo "Installation complete:
+echo "✅ Installation complete:
 - App:                $DEST_APP
 - Restore script:     $DEST_RESTORE
 - Venv:               $VENV_DIR
